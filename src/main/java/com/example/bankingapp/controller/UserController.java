c6019af1117933b905757c8ef33698f213726446
rathnavelvija <rathnavelvija@gmail.com>
2024-03-21 21:43:08 +0530

    Updated Spring Security

--- src/main/java/com/example/bankingapp/controller/UserController.java
+++ src/main/java/com/example/bankingapp/controller/UserController.java
@@ -2,14 +2,16 @@ package com.example.bankingapp.controller;
 
 import com.example.bankingapp.model.User;
 import com.example.bankingapp.request.LoginRequest;
+import com.example.bankingapp.security.AuthRequest;
 import com.example.bankingapp.service.impl.UserServiceImpl;
+import com.example.bankingapp.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -18,6 +20,12 @@ public class UserController {
 
     @Autowired
     UserServiceImpl userServiceImpl;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
@@ -29,5 +37,25 @@ public class UserController {
     public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
         User user = userServiceImpl.login(loginRequest.getUsername(), loginRequest.getPassword());
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/authenticate")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+            if(authentication.isAuthenticated()){
+                return jwtUtil.generateToken(authRequest.getUsername());
+            }
+
+        }catch (Exception e){
+            throw new Exception("invalid username and password");
+        }
+
+
+        return "Invalid Creds";
     }
 }
---
+++ src/main/java/com/example/bankingapp/security/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.example.bankingapp.security;
+
+import com.example.bankingapp.model.User;
+import com.example.bankingapp.repo.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles("admin","normalUser")
+                .build();
+    }
+}
